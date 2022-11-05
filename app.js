49d215eb68b143ff916e6e41c991412e8a934c63
pankhuri813 <112609769+pankhuri813@users.noreply.github.com>
2022-11-05 23:59:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const instruction = document.getElementById("instruction");
+
+instruction.onclick = () => {
+location.href="./instruction.html";
+};
+
+const StartButton = document.getElementById("StartButton");
+
+StartButton.onclick = () => {
+    location.href="./game.html";
+}
---
+++ gameover.js
@@ -0,0 +1,5 @@
+const Playagain = document.getElementById("Playagain");
+
+Playagain.onclick = () => {
+location.href="./index.html";
+};
